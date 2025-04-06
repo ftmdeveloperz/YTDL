8ff5710cf6fd9ed8b585c0e2c96f6e841ae4e8fb
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-04-06 12:09:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ import os
 SESSION = "ftmbotzx"
 API_ID = int(os.getenv("API_ID", 22141398))
 API_HASH = os.getenv("API_HASH", '0c8f8bd171e05e42d6f6e5a6f4305389')
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8105194942:AAEWuvVEXmGVTuuhbmTrZfn3PMz7yDDhHc0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8105194942:AAHisU5gxAc77O5qKoAZluI86w10s9MBV5o")
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1002613994353"))
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1002613994353"))
 PORT = int(os.getenv("PORT", "8080"))
